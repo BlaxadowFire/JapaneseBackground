f1c80de802fb3020b7a6f1805271d39f1e505134
BlaxadowFire <nando.kools@hotmail.com>
2019-11-20 14:19:12 +0100

    Initial commit

---
+++ wallpaper.ps1
@@ -0,0 +1,30 @@
+﻿$BasePath = "C:\Users\NKOOLS98\OneDrive - Conclusion\Afbeeldingen\Wallpapers\"
+ 
+$Day = Get-Date -Format dddd
+
+
+
+
+$FileName = $Day + ".jpg"
+$FilePath = $BasePath + $FileName
+
+Function Set-WallPaper($imagePath)
+ {
+    Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value $imagePath
+    rundll32.exe user32.dll, UpdatePerUserSystemParameters
+    RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True
+ }
+ Function Set-LockScreen($imagePath)
+ {
+    REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP /f
+    REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP /v LockScreenImagePath /t REG_SZ /d $imagePath /f
+    REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP /v LockScreenImageUrl /t REG_SZ /d $imagePath /f
+    REG ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\PersonalizationCSP /v LockScreenImageStatus /t REG_DWORD /d 1 /f
+}
+
+ 
+
+
+ Write-Output "$FilePath"
+ Set-WallPaper -imagePath "$FilePath"
+ Set-LockScreen -imagePath "$FilePath"
